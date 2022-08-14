34c524105e56346bd88af65149ebecedefd119c2
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-08-14 01:45:40 -0400

    Update generatesitemap.py

--- generatesitemap.py
+++ generatesitemap.py
@@ -222,8 +222,8 @@ def lastmod(f, timestamp_format) :
                     universal_newlines=True).stdout.strip()
     if len(mod) == 0 :
         mod = datetime.now().astimezone().replace(microsecond=0).isoformat()
-    if timestamp_format != "None":
-        mod = datetime.strptime(mod, '%Y-%m-%dT%H:%M:%S%z').strftime(timestamp_format)  
+    if timestamp_format != "1":
+        mod = datetime.strptime(mod, '%Y-%b-%d').strftime(timestamp_format)  
     return mod
 
 def urlstring(f, baseUrl, dropExtension=False) :
