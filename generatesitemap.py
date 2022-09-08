e19c66acc23b73ca3847522b63a9d8453b51f7ad
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-09-08 00:48:54 -0400

    Update generatesitemap.py

--- generatesitemap.py
+++ generatesitemap.py
@@ -212,7 +212,7 @@ def lastmod(f, date_only) :
                     universal_newlines=True).stdout.strip()
     if len(mod) == 0 :
         mod = datetime.now().astimezone().replace(microsecond=0).isoformat()
-    if date_only != "false":
+    if date_only:
         date_only = '%Y-%m-%d'
         mod = datetime.strptime(mod, '%Y-%m-%dT%H:%M:%S%z').strftime(date_only)  
     return mod
@@ -287,7 +287,7 @@ if __name__ == "__main__" :
     sitemapFormat = sys.argv[5]
     additionalExt = set(sys.argv[6].lower().replace(",", " ").replace(".", " ").split())
     dropExtension = sys.argv[7]=="true"
-    date_only = sys.argv[8]	
+    date_only = sys.argv[8]
 	
     os.chdir(websiteRoot)
     blockedPaths = parseRobotsTxt()
